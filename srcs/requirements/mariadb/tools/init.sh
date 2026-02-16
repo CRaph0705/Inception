6dd2e575e4a4000bfa17a23be03401c8f9663d70
Raphael Cochrane <raphael.cochrane94@gmail.com>
2026-02-16 23:17:26 +0100

    mariadb and wp init script added

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 5
+
+mysql -u root << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
