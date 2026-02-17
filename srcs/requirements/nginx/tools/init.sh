805c84d84cd1d2f0a60102fab4661fc082f3c548
Raphael Cochrane <raphael.cochrane94@gmail.com>
2026-02-17 16:02:46 +0100

    docker compose updated, nginx conf fixed

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -4,3 +4,10 @@ set -e
 
 mkdir -p /etc/nginx/ssl
 
+openssl req -x509 -nodes -days 365 \
+    -newkey rsa:2048 \
+    -keyout /etc/nginx/ssl/nginx.key \
+    -out /etc/nginx/ssl/nginx.crt \
+    -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=student/CN=rcochran.42.fr"
+
+exec nginx -g "daemon off;"
