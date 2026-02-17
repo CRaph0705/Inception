2a082774f8a61177af67a598e3fbe54a38a773fd
Raphael Cochrane <raphael.cochrane94@gmail.com>
2026-02-17 15:39:32 +0100

    docker compose updated

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -11,5 +11,25 @@ openssl req -x509 -nodes -days 365 \
     -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=student/CN=${DOMAIN_NAME}"
 cd /var/www/html
 
+wait-for mariadb:3306 --timeout=30 --strict -- echo "MariaDB is up"
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp core download --allow-root
+    wp config create \
+        --dbname=${MYSQL_DATABASE} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=mariadb \
+        --allow-root
+
+    wp core install \
+        --url=https://${DOMAIN_NAME} \
+        --title="Inception" \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL} \
+        --allow-root
+fi
+
 
 exec nginx -g "daemon off;"
