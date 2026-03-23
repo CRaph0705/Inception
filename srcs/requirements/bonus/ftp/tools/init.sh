f27680764f5bc259211a5d7253e51172850308cf
rcochran <rcochran@Inception.myguest.virtualbox.org>
2026-03-23 16:36:20 +0100

    ftp permissions fixed

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
 
+UPLOAD_DIR="/var/www/wordpress/wp-content/uploads"
+
+# Prepare uploads folder
+mkdir -p $UPLOAD_DIR
+
 # Create FTP user if missing
 if ! id "$FTP_USER" >/dev/null 2>&1; then
     adduser --disabled-password --gecos "" "$FTP_USER"
     echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+    echo $FTP_USER >> /etc/vsftpd.userlist
 fi
 
-# Prepare uploads folder
-mkdir -p /var/www/wordpress/wp-content/uploads
+usermod -aG www-data $FTP_USER
 
 # Set ownership and permissions for FTP user
-chown -R "$FTP_USER:$FTP_USER" /var/www/wordpress/wp-content/uploads
-chmod -R 775 /var/www/wordpress/wp-content/uploads  # owner+group can write
+chown -R www-data:www-data $UPLOAD_DIR
+chmod -R 2775 $UPLOAD_DIR
+#chmod g+s $UPLOAD_DIR
 
-# Set group sticky bit so new files inherit the group >> avoid permissions issues later
-chmod g+s /var/www/wordpress/wp-content/uploads
+echo "ftp folder created"
+mkdir -p $UPLOAD_DIR/ftp
+echo "permissions given"
+chown $FTP_USER:www-data $UPLOAD_DIR/ftp
+chmod 2775 $UPLOAD_DIR/ftp
+#local_root=$UPLOAD_DIR/ftp
+
 
 # Prepare vsftpd runtime folder
 mkdir -p /var/run/vsftpd/empty
